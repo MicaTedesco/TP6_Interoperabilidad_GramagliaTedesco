971906a2dd0b2aa548e1a42c9c3b04976a7bb1be
MicaTedesco <163702890+MicaTedesco@users.noreply.github.com>
2025-11-19 13:55:41 -0300

    update

--- workflow_final.py
+++ workflow_final.py
@@ -19,13 +19,14 @@ print(f"URL → https://hapi.fhir.org/baseR4/Patient/{patient_id}\n")
 print("="*80)
 print("CREANDO PROCEDURE")
 print("• SNOMED CT 225358003 → Curación de herida")
-print("• SNOMED CT 284549007 → Herida cortante")
+print("• SNOMED CT 10960321000119104 → Herida cortante en antebrazo izquierdo")
 print("• Incluye performer, encounter, reasonCode, outcome, note")
-print("• DNI argentino como subject.identifier\n")
+print("• DNI argentino como identifier\n")
 
 proc = create_procedure_resource(patient_id, dni)
 proc_id = send_resource_to_hapi_fhir(proc, "Procedure")
 
 print("="*80)
 print("PROCEDURE CREADO CON ÉXITO")
+
 print(f"URL → https://hapi.fhir.org/baseR4/Procedure/{proc_id}")
