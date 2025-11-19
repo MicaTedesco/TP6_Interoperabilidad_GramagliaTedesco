1f3c0f2b4b353b1d392e6cf4677af2bad1907f58
MicaTedesco <163702890+MicaTedesco@users.noreply.github.com>
2025-11-19 13:24:46 -0300

    Add files via upload

---
+++ workflow_final.py
@@ -0,0 +1,31 @@
+import requests
+from create_procedure import create_procedure_resource
+from base import send_resource_to_hapi_fhir
+
+dni = "42843082"
+url = f"https://hapi.fhir.org/baseR4/Patient?identifier=http://www.renaper.gob.ar/dni|{dni}"
+
+print("BUSCANDO PACIENTE CON DNI: ", dni)
+print("\n")
+resp = requests.get(url).json()
+
+if resp["total"] == 0:
+    print("No se encontró el paciente.")
+    exit()
+
+patient_id = resp["entry"][0]["resource"]["id"]
+print(f"PACIENTE ENCONTRADO")
+print(f"URL → https://hapi.fhir.org/baseR4/Patient/{patient_id}\n")
+print("="*80)
+print("CREANDO PROCEDURE")
+print("• SNOMED CT 225358003 → Curación de herida")
+print("• SNOMED CT 284549007 → Herida cortante")
+print("• Incluye performer, encounter, reasonCode, outcome, note")
+print("• DNI argentino como subject.identifier\n")
+
+proc = create_procedure_resource(patient_id, dni)
+proc_id = send_resource_to_hapi_fhir(proc, "Procedure")
+
+print("="*80)
+print("PROCEDURE CREADO CON ÉXITO")
+print(f"URL → https://hapi.fhir.org/baseR4/Procedure/{proc_id}")
