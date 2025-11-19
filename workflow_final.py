986faa8a3c1f94386433179978786224dd250b04
MicaTedesco <163702890+MicaTedesco@users.noreply.github.com>
2025-11-19 15:29:53 -0300

    update

--- workflow_final.py
+++ workflow_final.py
@@ -2,10 +2,15 @@ import requests
 from create_procedure import create_procedure_resource
 from base import send_resource_to_hapi_fhir
 
-dni = "42843082"
-url = f"https://hapi.fhir.org/baseR4/Patient?identifier=http://www.renaper.gob.ar/dni|{dni}"
+practitioner_id = "52960700"  
 
-print("BUSCANDO PACIENTE CON DNI: ", dni)
+fecha_procedimiento = "2024-11-20"
+
+dni_paciente = "42843082"
+url = f"https://hapi.fhir.org/baseR4/Patient?identifier=http://www.renaper.gob.ar/dni|{dni_paciente}"
+
+print("\n")
+print("BUSCANDO PACIENTE CON DNI: ", dni_paciente)
 print("\n")
 resp = requests.get(url).json()
 
@@ -18,15 +23,14 @@ print(f"PACIENTE ENCONTRADO")
 print(f"URL → https://hapi.fhir.org/baseR4/Patient/{patient_id}\n")
 print("="*80)
 print("CREANDO PROCEDURE")
-print("• SNOMED CT 225358003 → Curación de herida")
-print("• SNOMED CT 10960321000119104 → Herida cortante en antebrazo izquierdo")
-print("• Incluye performer, encounter, reasonCode, outcome, note")
-print("• DNI argentino como identifier\n")
+print("• Incluye: paciente, médico, procedimiento llevado a cabo, fecha del procedimiento, motivo, resultado y nota clínica detallada")
+print("• SNOMED CT 225358003 → Procedimiento: Curación de herida")
+print("• SNOMED CT 10960321000119104 → Motivo: Herida cortante en antebrazo izquierdo")
 
-proc = create_procedure_resource(patient_id, dni)
+proc = create_procedure_resource(patient_id, dni_paciente, practitioner_id, fecha_procedimiento)
 proc_id = send_resource_to_hapi_fhir(proc, "Procedure")
 
 print("="*80)
+print("\n")
 print("PROCEDURE CREADO CON ÉXITO")
-
 print(f"URL → https://hapi.fhir.org/baseR4/Procedure/{proc_id}")
